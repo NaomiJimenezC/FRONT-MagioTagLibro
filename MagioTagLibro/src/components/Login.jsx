e8d3eaf4d1caa025f998ef43963ee3a745b4557d
angel-arrieta <a.arrieta03soto@gmail.com>
2025-01-08 16:40:31 +0100

    login - boton y recuperar contraseña

--- MagioTagLibro/src/components/Login.jsx
+++ MagioTagLibro/src/components/Login.jsx
@@ -7,6 +7,12 @@ function Login(){
             <input id="password" type="password">
 
             </input>
+            <input id="enter" type="submit">
+                Log In
+            </input>
+            <a href="https://en.wikipedia.org/wiki/Filler_text">
+                I have forgotten my password
+            </a>
         </>
     )
 }
