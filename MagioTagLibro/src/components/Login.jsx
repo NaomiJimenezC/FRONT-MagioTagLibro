9db9e634087199989df6a4d234327e4e831b3609
angel-arrieta <a.arrieta03soto@gmail.com>
2025-01-08 10:49:05 +0100

    componente formulario Login jsx

---
+++ MagioTagLibro/src/components/Login.jsx
@@ -0,0 +1,14 @@
+function Login(){
+    return (
+        <>
+            <input id="user" type="text" title="user">
+
+            </input>
+            <input id="password" type="password">
+
+            </input>
+        </>
+    )
+}
+
+export default Login;
