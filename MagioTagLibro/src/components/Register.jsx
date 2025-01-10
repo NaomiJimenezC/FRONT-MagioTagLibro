1db98733c0ab52e1bf15a381ea75a336bb7e0d05
angel-arrieta <a.arrieta03soto@gmail.com>
2025-01-10 10:18:39 +0100

    componente formulario Register jsx

---
+++ MagioTagLibro/src/components/Register.jsx
@@ -0,0 +1,27 @@
+function Register(){
+    return (
+        <>
+        <input id="userName" type="text" title="username">
+
+        </input>
+        <input id="insertEmail" type="email" title="email">
+
+        </input>
+        <input id="birthDate" type="datetime-local">
+
+        </input>
+        <input id="createPassword" type="password">
+
+        </input>
+        <input id="confirmPassword" type="password">
+
+        </input>
+        <input id="termsOfService" type="checkbox" name="termsOfService" />
+        <input id="enter" type="submit">
+            Create Account
+        </input>
+</>
+)
+}
+
+export default Register;
