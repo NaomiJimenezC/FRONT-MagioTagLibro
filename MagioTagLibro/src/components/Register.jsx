3c2e7a079c856eaed236b09cd844a5a741609cc1
angel-arrieta <a.arrieta03soto@gmail.com>
2025-01-12 19:03:10 +0100

    register - validación

--- MagioTagLibro/src/components/Register.jsx
+++ MagioTagLibro/src/components/Register.jsx
@@ -1,26 +1,74 @@
+import * as Formik from 'formik'
+import { useFormik } from 'formik'
+import * as Yup from 'yup'
+
+const registerForm = () => {
+    const formik = useFormik({
+        initialValues : {
+            userName: "",
+            email: "",
+            birthDate: "",
+            newPassword: "",
+            confirmPassword: "",
+        },
+        onSubmit: values => {
+            alert(JSON.stringify(values, null, 2))
+        }
+    })
+
+    const validationSchema = Yup.object({
+        userName: Yup.string()
+            .required("Username is required")
+            .min(5, "Username must be at least 5 characters"),
+        email: Yup.string()
+            .required("Email is required")
+            .email("Email is required"),
+        password: Yup.string()
+            .required("Password is required")
+            .min(6, "Password must be at least 6 characters"),
+        confirmPassword: Yup.string()
+            .required("Confirm Password is required")
+            .oneOf([Yup.ref("password"), null], "Passwords must match")
+    })
+}
+
 function Register(){
     return (
-        <>
-            <input id="userName" type="text" title="username">
+        <Formik
+            initialValues={initialValues}
+            validationSchema={validationSchema}
+        >
+        <form method="get" onSubmit={registerForm.formik.handleSubmit}>
+            <p>Username</p>
+            <input id="userName" type="text" name="username" onChange={registerForm.formik.handleChange}
+                   value={registerForm.formik.initialValues.userName}>
 
             </input>
-            <input id="insertEmail" type="email" title="email">
+            <p>E-mail</p>
+            <input id="insertEmail" type="email" name="email" onChange={registerForm.formik.handleChange}
+                   value={registerForm.formik.initialValues.email}>
 
             </input>
-            <input id="birthDate" type="datetime-local">
+            <p>Birth Date</p>
+            <input id="birthDate" type="datetime-local" onChange={registerForm.formik.handleChange}
+                   value={registerForm.formik.initialValues.birthDate}>
 
             </input>
-            <input id="createPassword" type="password">
+            <p>Create Password</p>
+            <input id="createPassword" type="password" onChange={registerForm.formik.handleChange}
+                   value={registerForm.formik.initialValues.newPassword}>
 
             </input>
-            <input id="confirmPassword" type="password">
+
+            <input id="confirmPassword" type="password" onChange={registerForm.formik.handleChange}
+                   value={registerForm.formik.initialValues.confirmPassword}>
 
             </input>
-            <input id="termsOfService" type="checkbox" name="termsOfService" />
-            <input id="enter" type="submit">
-                Create Account
-            </input>
-        </>
+            <input id="termsOfService" type="checkbox" name="termsOfService" onChange={registerForm.formik.handleChange}/>
+
+            <input id="enter" type="submit" value="Create Account" onChange={registerForm.formik.submit}/>
+        </form>
+        </Formik>
     )
 }
 
