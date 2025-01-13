0e0bc9e9e31cefb7207d0e72b5a4a3ceef911e97
angel-arrieta <a.arrieta03soto@gmail.com>
2025-01-13 19:16:58 +0100

    register - arreglar formulario (a medias)

--- MagioTagLibro/src/components/Registerform.jsx
+++ MagioTagLibro/src/components/Registerform.jsx
@@ -1,67 +1,89 @@
-import * as Formik from 'formik'
-import { useFormik } from 'formik'
-import * as Yup from 'yup'
+import { Formik, Form, Field, ErrorMessage, useFormik } from 'formik'
+import * as yup from 'yup'
 
 const registerForm = () => {
     const formik = useFormik({
         initialValues : {
             userName: "",
             email: "",
-            newPassword: "",
+            password: "",
             confirmPassword: "",
-        },
-        onSubmit: values => {
-            alert(JSON.stringify(values, null, 2))
         }
     })
 
-    const validationSchema = Yup.object({
-        userName: Yup.string()
+    const validationSchema = yup.object({
+        userName: yup.string()
             .required("Username is required")
             .min(5, "Username must be at least 5 characters"),
-        email: Yup.string()
+        email: yup.string()
             .required("Email is required")
             .email("Email is required"),
-        password: Yup.string()
+        password: yup.string()
             .required("Password is required")
             .min(6, "Password must be at least 6 characters"),
-        confirmPassword: Yup.string()
+        confirmPassword: yup.string()
             .required("Confirm Password is required")
-            .oneOf([Yup.ref("password"), null], "Passwords must match")
+            .oneOf([yup.ref("password"), null], "Passwords must match"),
     })
+    const handleSubmit = async (values, { setSubmitting, resetForm }) => {
+        const { username, email, password } = values
+    }
 }
 
 function Registerform(){
     return (
         <Formik
-            initialValues={initialValues}
-            validationSchema={validationSchema}
-        >
-        <form method="get" onSubmit={registerForm.formik.handleSubmit}>
-            <p>Username</p>
-            <input id="userName" type="text" name="username" onChange={registerForm.formik.handleChange}
-                   value={registerForm.formik.initialValues.userName}>
+            initialValues={registerForm.initialValues}
+            validationSchema={registerForm.validationSchema}
+            onSubmit={registerForm.handleSubmit}
+        >   {({ isSubmitting }) => (
 
-            </input>
-            <p>E-mail</p>
-            <input id="insertEmail" type="email" name="email" onChange={registerForm.formik.handleChange}
-                   value={registerForm.formik.initialValues.email}>
+            <form method="get" onSubmit={registerForm.handleSubmit}>
+                <fieldset name="userName">
+                    <label htmlFor="userName">Username </label>
+                    <input id="userName" type="text" name="username" onChange={registerForm.handleChange}
+                           value={registerForm.initialValues}>
+                    </input>
+                    <ErrorMessage name="userName" component="fieldset" className="error" />
+                </fieldset>
 
-            </input>
-            <p>Create Password</p>
-            <input id="createPassword" type="password" onChange={registerForm.formik.handleChange}
-                   value={registerForm.formik.initialValues.newPassword}>
+                <fieldset name="email">
+                    <label htmlFor="email">E-mail </label>
+                    <input id="insertEmail" type="email" name="email" onChange={registerForm.handleChange}
+                           value={registerForm.initialValues}>
+                    </input>
+                    <ErrorMessage name="email" component="fieldset" className="error" />
+                </fieldset>
 
-            </input>
+                <fieldset name="password">
+                    <label htmlFor="password">Password </label>
+                    <input id="createPassword" type="password" onChange={registerForm.handleChange}
+                           value={registerForm.initialValues}>
+                    </input>
+                    <ErrorMessage name="password" component="fieldset" className="error" />
+                </fieldset>
 
-            <input id="confirmPassword" type="password" onChange={registerForm.formik.handleChange}
-                   value={registerForm.formik.initialValues.confirmPassword}>
+                <fieldset name="confirmPassword">
+                    <label htmlFor="confirmPassword">Confirm Password </label>
+                    <input id="confirmPassword" type="password" onChange={registerForm.handleChange}
+                           value={registerForm.initialValues}>
+                    </input>
+                    <ErrorMessage name="confirmPassword" component="fieldset" className="error" />
+                </fieldset>
 
-            </input>
-            <input id="termsOfService" type="checkbox" name="termsOfService" onChange={registerForm.formik.handleChange}/>
+                <fieldset name="termsOfService">
+                    <label htmlFor="termsOfService">I agree to the Terms of Service: </label>
+                    <input id="termsOfService" type="checkbox" name="termsOfService"
+                           onChange={registerForm.handleChange}/>
+                    <ErrorMessage name="termsOfService" component="fieldset" className="error" />
+                </fieldset>
 
-            <input id="enter" type="submit" value="Create Account" onChange={registerForm.formik.submit}/>
-        </form>
+                <fieldset name="submit">
+                    <input id="submit" type="submit" value="Create Account" onChange={registerForm.submit}/>
+                </fieldset>
+
+            </form> )
+            }
         </Formik>
     )
 }
