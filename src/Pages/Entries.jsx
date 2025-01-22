df01bdae1b4efa72b5cecdb992cc94ae53c35c83
naomi_love_u_3 <113902606+NaomiJimenezC@users.noreply.github.com>
2025-01-22 16:44:27 +0100

    Update Entries.jsx

--- src/Pages/Entries.jsx
+++ src/Pages/Entries.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import axios from 'axios';
-import Navbar from "../Components/Navbar.jsx";
+import Navbar from "../Components/NavBar.jsx";
 import useCheckTodayEntry from "../Functionalities/todayEntries.jsx";
 import Button from "../Components/Button.jsx"; // Asumiendo que tienes un componente Button
 
