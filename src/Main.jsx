454e44c450e6863a7bd053a1d6e19861f5d966ba
NaomiJimenezC <ijimenez@iesrafaelalberti.es>
2025-02-04 12:21:06 +0100

    import fixed (seriously, again?)

--- src/Main.jsx
+++ src/Main.jsx
@@ -4,7 +4,7 @@ import { RouterProvider } from "react-router-dom";
 import { ThemeProvider } from "./Context/ThemeContext"; 
 import { AuthProvider } from "./Context/AuthContext"; 
 import { router } from "./Router/Index.jsx";
-import './Sass/globalLayout.scss'; 
+import './Sass/GlobalLayout.scss';
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
